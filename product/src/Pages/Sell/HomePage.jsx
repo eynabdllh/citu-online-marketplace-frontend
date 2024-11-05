6411cba9b6eff3d8df4c28e13c658170ebdeabc9
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-05 09:16:53 +0800

    Added HomePage

---
+++ product/src/Pages/Sell/HomePage.jsx
@@ -0,0 +1,11 @@
+//import { Box, Button, Typography, Card, CardContent, Grid, CardMedia } from '@mui/material';
+import {Typography}from '@mui/material';
+const HomePage = () => {
+return (
+        <Typography variant="h6" sx={{ marginTop: '16px' }}>
+          Mic Test, Mic Test
+        </Typography>
+  );
+};
+
+export default HomePage;
