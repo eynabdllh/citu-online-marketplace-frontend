a9c6f11a46749b485286700e78ed5fb9e4696f1b
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-05 09:16:43 +0800

    Added BuyPage

---
+++ product/src/Pages/Sell/BuyPage.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Typography, Card, CardContent, Grid, CardMedia } from '@mui/material';
+import axios from 'axios';
+import '../../App.css';
