86a4e39760208dcfa158ac4709834fafd70500d0
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-08 07:51:09 +0800

    Delete (Wrong folder)

--- product/src/Pages/Sell/BuyPage.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, Button, Typography, Card, CardContent, Grid, CardMedia } from '@mui/material';
-import axios from 'axios';
-import '../../App.css';
