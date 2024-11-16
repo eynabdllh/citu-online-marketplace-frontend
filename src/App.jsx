cf73a0ddf2747788f42086c9fcae99bd0daf960c
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-17 03:33:38 +0800

    implemented Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import SellPage from './Pages/Sell/SellPage';
@@ -11,32 +13,33 @@ import Register from './Pages/LoginRegister/Register';
 import Login from './Pages/LoginRegister/Login';
 import UserAccount from './Pages/Profile/UserAccount';
 import UserProfile from './Pages/Profile/UserProfile';
+import { AuthProvider } from './contexts/AuthContext'; 
 import './App.css';
 
 const App = () => {
   const location = useLocation();
 
   return (
-    <div>
-      {location.pathname !== '/' && location.pathname !== '/register' && <MarketplaceHeader />}
-
-      <Routes>
-      <Route path="/" element={<Login />} /> 
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/account" element={<UserAccount />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/buy" element={<BuyPage />} />
-        <Route path="/buy/product/:code" element={<ViewProduct section="Buy" />} />
-        <Route path="/sell" element={<SellPage />} />
-        <Route path="/sell/product/:code" element={<ViewProduct section="Sell" />} />
-        <Route path="/addnewproduct" element={<AddProductForm />} />
-        <Route path="/product/:code" element={<ViewProduct />} />
-        <Route path="/update/:code" element={<UpdateProductForm />} />
-        {/* <Route path="/message" element={<MessagePage />} />
-        <Route path="/feedback" element={<FeedbackPage />} /> */}
-      </Routes>
-    </div>
+    <AuthProvider> 
+      <div>
+        {location.pathname !== '/' && location.pathname !== '/register' && <MarketplaceHeader />}
+        
+        <Routes>
+          <Route path="/" element={<Login />} /> 
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/account" element={<UserAccount />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/buy" element={<BuyPage />} />
+          <Route path="/buy/product/:code" element={<ViewProduct section="Buy" />} />
+          <Route path="/sell" element={<SellPage />} />
+          <Route path="/sell/product/:code" element={<ViewProduct section="Sell" />} />
+          <Route path="/addnewproduct" element={<AddProductForm />} />
+          <Route path="/product/:code" element={<ViewProduct />} />
+          <Route path="/update/:code" element={<UpdateProductForm />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 };
 
--- src/Pages/LoginRegister/Login.jsx
+++ src/Pages/LoginRegister/Login.jsx
@@ -1,32 +1,25 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import { useAuth } from '../../contexts/AuthContext'; 
 import '../../assets/static/Login.css';
 import logo from '../../assets/img/logocit-1.png';
 import cit from '../../assets/img/cit-1.jpg';
 
 const Login = () => {
-    const [credentials, setCredentials] = useState({ username: '', password: '' }); 
+    const [credentials, setCredentials] = useState({ username: '', password: '' });
     const [errorMessage, setErrorMessage] = useState('');
+    const { login } = useAuth(); // Use login function from AuthContext
     const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        console.log('Logging in with credentials:', credentials);
         try {
             const response = await axios.post('http://localhost:8080/api/seller/login', credentials);
-            console.log('Login Successful', response.date);
+            const userData = response.data;
 
-            const { firstName, lastName, address, contactNo, email, username, password} = response.data;
-            sessionStorage.setItem('username', username);
-            sessionStorage.setItem('password', password);
-            sessionStorage.setItem('firstName', firstName);
-            sessionStorage.setItem('lastName', lastName);
-            sessionStorage.setItem('address', address);
-            sessionStorage.setItem('contactNo', contactNo);
-            sessionStorage.setItem('email', email);
-
-            setErrorMessage('')
+            // Call login from AuthContext with the user data
+            login(userData);
             navigate('/home');
         } catch (error) {
             setErrorMessage('Invalid username or password');
@@ -34,48 +27,43 @@ const Login = () => {
         }
     };
 
-  return (
-    <div className="login-container">
-      <div className="form-container">
-        <img src={logo} alt="Logo" className="logo" /> 
-        <h2>Welcome to CIT-U Marketplace</h2>
-        <span>Login to your account</span>
-
-        <form onSubmit={handleLogin}>
-          {errorMessage && 
-            <div className="error-message">{errorMessage}</div>
-          }
-
-        <div className="form-group">
-            <input 
-            type="text" 
-            name="username" 
-            placeholder='Username'
-            value={credentials.username}
-            onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-            />
-          </div>
-          
-          <div className="form-group">
-            <input 
-            type="password" 
-            name="password" 
-            placeholder='Password'
-            value={credentials.password}
-            onChange={(e) => setCredentials({ ...credentials, password: e.target.value})}
-            />
-          </div>
-          <div className="button-container">
-            <button type="submit">Login</button>
-            <p className="signup-text">Don't have an account?  <Link to="/register">Sign up for free</Link></p>
-          </div>
-        </form>
-      </div>
-      <div className="image-container">
-        <img src={cit} alt="Registration" className="registration-image" /> 
-      </div>
-    </div>
-  );
+    return (
+        <div className="login-container">
+            <div className="form-container">
+                <img src={logo} alt="Logo" className="logo" />
+                <h2>Welcome to CIT-U Marketplace</h2>
+                <span>Login to your account</span>
+                <form onSubmit={handleLogin}>
+                    {errorMessage && <div className="error-message">{errorMessage}</div>}
+                    <div className="form-group">
+                        <input 
+                            type="text" 
+                            name="username" 
+                            placeholder="Username"
+                            value={credentials.username}
+                            onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="Password"
+                            value={credentials.password}
+                            onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                        />
+                    </div>
+                    <div className="button-container">
+                        <button type="submit">Login</button>
+                        <p className="signup-text">Don't have an account? <Link to="/register">Sign up for free</Link></p>
+                    </div>
+                </form>
+            </div>
+            <div className="image-container">
+                <img src={cit} alt="Registration" className="registration-image" />
+            </div>
+        </div>
+    );
 };
 
 export default Login;
