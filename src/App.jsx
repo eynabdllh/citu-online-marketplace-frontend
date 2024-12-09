1095e5751ffedc2bd013874f5e8e0d23bbb36b68
anamgndia <abdullanamerah@gmail.com>
2024-12-09 16:50:32 +0800

    [Ana] added toast

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import ProductApproval from './Pages/Admin/ProductApproval/ProductApproval';
 import AdminSettings from './Pages/Admin/AdminSettings';
 import AdminNotifications from './Pages/Admin/AdminNotifications';
 import UserNotifications from './Pages/Notifications/UserNotifications';
+import { Toaster } from 'react-hot-toast';
 
 import './App.css';
 
@@ -64,72 +65,75 @@ const App = () => {
   const isAdminRoute = location.pathname.startsWith('/admin') && location.pathname !== '/admin';
 
   return (
-    <AuthProvider>
-      <div>
-        {!location.pathname.startsWith('/admin') && 
-         location.pathname !== '/' && 
-         location.pathname !== '/register' && 
-         location.pathname !== '/admin' &&
-         <MarketplaceHeader />}
-        
-        {isAdminRoute && <AdminHeader />}
-        
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin" element={<AdminLogin />} />
+    <>
+      <Toaster />
+      <AuthProvider>
+        <div>
+          {!location.pathname.startsWith('/admin') && 
+           location.pathname !== '/' && 
+           location.pathname !== '/register' && 
+           location.pathname !== '/admin' &&
+           <MarketplaceHeader />}
+          
+          {isAdminRoute && <AdminHeader />}
+          
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<AdminLogin />} />
 
-          {/* Admin Routes */}
-          <Route path="/admin/dashboard" element={
-            <ProtectedAdminRoute>
-              <Dashboard />
-            </ProtectedAdminRoute>
-          } />
-          <Route path="/admin/users" element={
-            <ProtectedAdminRoute>
-              <UserManagement />
-            </ProtectedAdminRoute>
-          } />
-          <Route path="/admin/productsellers" element={
-            <ProtectedAdminRoute>
-              <ProductManagement />
-            </ProtectedAdminRoute>
-          } />
-          <Route path="/admin/approvals" element={
-            <ProtectedAdminRoute>
-              <ProductApproval />
-            </ProtectedAdminRoute>
-          } />
-          <Route path="/admin/settings" element={
-            <ProtectedAdminRoute>
-              <AdminSettings />
-            </ProtectedAdminRoute>
-          } />
-          <Route path="/admin/notifications" element={
-            <ProtectedAdminRoute>
-              <AdminNotifications />
-            </ProtectedAdminRoute>
-          } />
+            {/* Admin Routes */}
+            <Route path="/admin/dashboard" element={
+              <ProtectedAdminRoute>
+                <Dashboard />
+              </ProtectedAdminRoute>
+            } />
+            <Route path="/admin/users" element={
+              <ProtectedAdminRoute>
+                <UserManagement />
+              </ProtectedAdminRoute>
+            } />
+            <Route path="/admin/productsellers" element={
+              <ProtectedAdminRoute>
+                <ProductManagement />
+              </ProtectedAdminRoute>
+            } />
+            <Route path="/admin/approvals" element={
+              <ProtectedAdminRoute>
+                <ProductApproval />
+              </ProtectedAdminRoute>
+            } />
+            <Route path="/admin/settings" element={
+              <ProtectedAdminRoute>
+                <AdminSettings />
+              </ProtectedAdminRoute>
+            } />
+            <Route path="/admin/notifications" element={
+              <ProtectedAdminRoute>
+                <AdminNotifications />
+              </ProtectedAdminRoute>
+            } />
 
-          {/* Protected User Routes */}
-          <Route path="/home" element={<ProtectedUserRoute><HomePage /></ProtectedUserRoute>} />
-          <Route path="/buy" element={<ProtectedUserRoute><BuyPage /></ProtectedUserRoute>} />
-          <Route path="/sell" element={<ProtectedUserRoute><SellPage /></ProtectedUserRoute>} />
-          <Route path="/profile" element={<ProtectedUserRoute><Profile /></ProtectedUserRoute>} />
-          <Route path="/likes" element={<ProtectedUserRoute><Likes /></ProtectedUserRoute>} />
-          <Route path="/account" element={<ProtectedUserRoute><Settings /></ProtectedUserRoute>} />
-          <Route path="/message" element={<ProtectedUserRoute><Message /></ProtectedUserRoute>} />
-          <Route path="/notifications" element={<ProtectedUserRoute><UserNotifications /></ProtectedUserRoute>} />
-          <Route path="/buy/product/:code" element={<ViewProduct section="Buy" />} />
-          <Route path="/addnewproduct" element={<AddProductForm />} />
-          <Route path="/product/:code" element={<ViewProduct />} />
-          <Route path="sell/product/:code" element={<ViewforSeller />} /> 
-          <Route path="/update/:code" element={<UpdateProductForm />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </AuthProvider>
+            {/* Protected User Routes */}
+            <Route path="/home" element={<ProtectedUserRoute><HomePage /></ProtectedUserRoute>} />
+            <Route path="/buy" element={<ProtectedUserRoute><BuyPage /></ProtectedUserRoute>} />
+            <Route path="/sell" element={<ProtectedUserRoute><SellPage /></ProtectedUserRoute>} />
+            <Route path="/profile" element={<ProtectedUserRoute><Profile /></ProtectedUserRoute>} />
+            <Route path="/likes" element={<ProtectedUserRoute><Likes /></ProtectedUserRoute>} />
+            <Route path="/account" element={<ProtectedUserRoute><Settings /></ProtectedUserRoute>} />
+            <Route path="/message" element={<ProtectedUserRoute><Message /></ProtectedUserRoute>} />
+            <Route path="/notifications" element={<ProtectedUserRoute><UserNotifications /></ProtectedUserRoute>} />
+            <Route path="/buy/product/:code" element={<ViewProduct section="Buy" />} />
+            <Route path="/addnewproduct" element={<AddProductForm />} />
+            <Route path="/product/:code" element={<ViewProduct />} />
+            <Route path="sell/product/:code" element={<ViewforSeller />} /> 
+            <Route path="/update/:code" element={<UpdateProductForm />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </>
   );
 };
 
